d63f649bcb7a9f8de8a1092c142c16823ea6dd24
mblough <mblough7@gmail.com>
2015-02-10 10:47:41 -0500

    Added routes and controller methods for movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,38 @@
+class MoviesController < ApplicationController
+	before_filter :authenticate_user!, only: [:create, :update, :destroy]
+
+	respond_to :json
+
+	def index
+		@user = User.find(params[:user_id])
+		@movies = @user.movies
+		render json: @movies
+	end
+
+	def create
+		respond_with Movie.create(movie_params.merge(user_id: current_user.id))
+	end
+
+	def show
+		respond_with Movie.find(params[:id])
+	end
+
+	def update
+		Movie.find(params[:id]).update_attributes(movie_params)
+		render nothing: true, status: 204
+	end
+
+	def destroy
+		Movie.find(params[:id]).destroy
+		render nothing: true, status: 204
+	end
+
+	def as_json(options = {})
+		super(options.merge(include: :user))
+	end
+
+	private
+	def movie_params
+		params.require(:movie).permit(:name, :watched, :rating, :comment)
+	end
+end
