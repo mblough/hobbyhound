07bc84c5e45cf455876db907680d3cc7fcc13c7d
mblough <mblough7@gmail.com>
2015-02-12 07:56:20 -0500

    Added movies assets folder

---
+++ app/assets/javascripts/movies/movies.js
@@ -0,0 +1,20 @@
+angular.module('hobbyhound')
+.factory('movies', ['$http', function($http) {
+	var m = {
+		movies: []
+	};
+
+	m.getAll = function(userid) {
+		return $http.get('/users/' + userid + '/movies.json').success(function(data) {
+			angular.copy(data, m.movies);
+		});
+	};
+
+	m.create = function(userid, movie) {
+		return $http.post('/users/' + userid + '/movies.json', movie).success(function(data) {
+			m.movies.push(data);
+		});
+	};
+
+	return m;
+}])
