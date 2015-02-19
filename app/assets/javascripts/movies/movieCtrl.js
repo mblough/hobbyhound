3d8123928dd2e65c902d0f23bb35aa2120016e64
mblough <mblough7@gmail.com>
2015-02-19 09:37:44 -0500

    added base html templates and controller for movies

--- app/assets/javascripts/movies/movieCtrl.js
+++ app/assets/javascripts/movies/movieCtrl.js
@@ -1,4 +1,14 @@
 angular.module('hobbyhound')
 .controller('MovieCtrl', ['$scope', 'users', 'movies', function($scope, users, movies) {
 	$scope.profile = users.prof;
+
+	$scope.movie = {};
+	$scope.max = 10;
+
+	$scope.hoveringOver = function(value) {
+		$scope.overStar = value;
+		$scope.percent = 100 * (value / $scope.max);
+	};
+
+	
 }]);
