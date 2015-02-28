1e2c73a46c12916fe52b68546f32ae113b8a1a4c
mblough <mblough7@gmail.com>
2015-02-28 07:16:54 -0500

    Added game model to server/db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210125419) do
+ActiveRecord::Schema.define(version: 20150228121219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name"
+    t.string   "system"
+    t.boolean  "beaten"
+    t.boolean  "playing"
+    t.integer  "rating"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
