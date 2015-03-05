eb7b78f1ea8c21201f910bb749670a9b5905f9fa
mblough <mblough7@gmail.com>
2015-03-05 08:24:26 -0500

    Create api routes for games

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resources :movies, shallow: true
   end
 
+  resources :users do
+    resources :games, shallow: true
+  end
+
   resources :profiles, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
