63b7177c55fd79b7cb2d4ebc994b336b72178436
mblough <mblough7@gmail.com>
2015-03-05 08:23:58 -0500

    Added reference/foreign key for users to games

---
+++ db/migrate/20150305130758_add_user_ref_to_games.rb
@@ -0,0 +1,6 @@
+class AddUserRefToGames < ActiveRecord::Migration
+  def change
+    add_reference :games, :user, index: true
+    add_foreign_key :games, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228121219) do
+ActiveRecord::Schema.define(version: 20150305130758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 20150228121219) do
     t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
@@ -61,5 +64,6 @@ ActiveRecord::Schema.define(version: 20150228121219) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "games", "users"
   add_foreign_key "movies", "users"
 end
