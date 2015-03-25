edbc891dd098d21766cb3a9fcc057ed1f92795c6
mblough <mblough7@gmail.com>
2015-03-25 08:39:58 -0400

    Added show routes to server side

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
     resources :books, shallow: true
   end
 
+  resources :users do
+    resources :shows, shallow: true
+  end
+
   resources :profiles, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
