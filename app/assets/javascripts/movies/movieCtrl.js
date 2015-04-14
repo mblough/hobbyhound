63250ee1636a567e22c9685c3ee962afd088ead3
mblough <mblough7@gmail.com>
2015-04-14 18:19:31 -0400

    Spacing

--- app/assets/javascripts/movies/movieCtrl.js
+++ app/assets/javascripts/movies/movieCtrl.js
@@ -2,15 +2,40 @@ angular.module('hobbyhound')
 .controller('MovieCtrl', ['$scope', 'users', 'movies', '$modal', '$location', 'dashboard', function($scope, users, movies, $modal, $location, dashboard) {
 	$scope.profile = users.prof;
 
-	$scope.movies = movies.movies;
+	$scope.allMovies = movies.movies;
 
 	$scope.movieProgress = dashboard.movieProgress;
 
 	$scope.rating = 0;
 	$scope.max = 5;
 
+	$scope.movies = $scope.allMovies;
+
 	$scope.findProgressType = function(progress) {
 		return dashboard.findProgressType(progress);
+	};
+
+	$scope.setFilter = function(filterType) {
+		$scope.movies = [];
+		switch(filterType) {
+			case 'all':
+				$scope.movies = $scope.allMovies;
+				break;
+			case 'watched':
+				for (var j = 0; j < $scope.allMovies.length; j++) {
+					if($scope.allMovies[j].watched) {
+						$scope.movies.push($scope.allMovies[j]);
+					}
+				}
+				break;
+			case 'unwatched':
+				for (var k = 0; k < $scope.allMovies.length; k++) {
+					if (!($scope.allMovies[k].watched)) {
+						$scope.movies.push($scope.allMovies[k]);
+					}
+				}
+				break;
+		}
 	};
 
 	$scope.hoveringOver = function(value) {
